6a205c1c150cb490108cc9388889b99f87b4c117
Manuel Hutter <manuel@hutter.io>
2024-07-17 09:27:45 +0200

    createdb: Always use primary
    The secondaries are in read-only after all
    
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- script/createdb.sh
+++ script/createdb.sh
@@ -6,11 +6,12 @@ NS="psql"
 CLUSTER="shared"
 ###
 
+pod="$(kubectl -n "$NS" get pod -o name -l "cnpg.io/cluster=${CLUSTER}" -l "cnpg.io/instanceRole=primary")"
 name="$1"
 password="$(pwgen -cns 64 1)"
 
-kubectl -n "$NS" exec -c postgres "${CLUSTER}-1" -- psql --command "CREATE USER ${name} WITH ENCRYPTED PASSWORD '$password';"
-kubectl -n "$NS" exec -c postgres "${CLUSTER}-1" -- psql --command "CREATE DATABASE ${name} OWNER ${name};"
+kubectl -n "$NS" exec -c postgres "$pod" -- psql --command "CREATE USER ${name} WITH ENCRYPTED PASSWORD '$password';"
+kubectl -n "$NS" exec -c postgres "$pod" -- psql --command "CREATE DATABASE ${name} OWNER ${name};"
 
 echo "Created new database"
 echo
